df05efb046da1e5fc8c494a9f4fe522ddb94a562
TwinMine <michaelrolo11@gmail.com>
2026-03-25 11:03:33 +0100

    add crypto zu hide password

---
+++ typescript/märz/25.03.2026 /passwordManager/functions/crypto.ts
@@ -0,0 +1,35 @@
+import { scryptSync, randomBytes, createCipheriv, createDecipheriv } from "crypto";
+const SALT = "fixed-salt"; 
+
+export function createKey(masterPassword: string) {
+  return scryptSync(masterPassword, SALT, 32);
+}
+
+
+
+export function encrypt(text: string, key: Buffer) {
+  const iv = randomBytes(16); 
+
+  const cipher = createCipheriv("aes-256-cbc", key, iv);
+
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+
+  return {
+    value: encrypted,
+    iv: iv.toString("hex"),
+  };
+}
+
+
+
+export function decrypt(encryptedData: string, ivHex: string, key: Buffer) {
+  const iv = Buffer.from(ivHex, "hex");
+
+  const decipher = createDecipheriv("aes-256-cbc", key, iv);
+
+  let decrypted = decipher.update(encryptedData, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+
+  return decrypted;
+}
---
+++ typescript/märz/25.03.2026 /passwordManager/types.ts
@@ -0,0 +1,15 @@
+export interface Password {
+  id: string,
+  name: string,
+  value: string;
+  strengh: string;
+  iv: string,
+  options: PasswordOptions;
+}
+
+export interface PasswordOptions {
+  uppercase: boolean;
+  includeNumbers: boolean;
+  includeSymbols: boolean;
+  length: number;
+}

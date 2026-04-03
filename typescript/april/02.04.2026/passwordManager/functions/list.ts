94797b3921eb88022a20f6a18c8857854987114d
TwinMine <michaelrolo11@gmail.com>
2026-04-03 15:28:09 +0200

    add 3 days and a new project

---
+++ typescript/april/02.04.2026/passwordManager/functions/list.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import type { Password, Storage } from "../types.ts";
+import { createKey, decrypt } from "./crypto.ts";
+
+export async function list(passwords: Password[], data: Storage) {
+
+  if (!data.masterPassword) {
+  throw new Error("Master-Passwort fehlt!");
+}
+
+  const key = createKey(data.masterPassword);
+
+    if (passwords.length === 0) {
+    console.log("Keine gespeicherten Passwörter vorhanden.");
+    return;
+  }
+  const list = passwords.map((p, i) => `${i + 1}.${p.name}: ${decrypt(p.value, p.iv, key)}`).join("\n");
+
+  await inquirer.prompt({
+    type: "select", 
+    name: "choice",
+    message: `Deine gespeicherten Passwörter:\n${list}\nFortfahren ->`,
+    choices: ["Fortfahren"]
+  });
+}
